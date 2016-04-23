518575d5229b44245724f8f1cc97a15bc14860ac
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2016-04-23 19:15:38 +0200

    Error or success of registration/login for users

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -1,8 +1,14 @@
 var controllers = angular.module('maris');
 
 
-controllers.controller('logInCtrl', ['$scope', '$log', '$http', '$mdDialog', '$mdMedia', function ($scope, $log, $http, $mdDialog, $mdMedia){
-    
+controllers.controller('logInCtrl', ['$scope', '$log', '$http', '$mdDialog', '$mdMedia', function ($scope, $log, $http, $mdDialog, $mdMedia) {
+
+    //Close the modal window
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+
+    //Show the modal window
     $scope.showLogIn = function (ev) {
         $mdDialog.show({
             controller: WordController,
@@ -12,86 +18,98 @@ controllers.controller('logInCtrl', ['$scope', '$log', '$http', '$mdDialog', '$m
             clickOutsideToClose: true
         });
     };
-    
 
-        
-}]);
+    function WordController($scope, $http, $mdDialog) {
 
-function WordController($scope,  $http) {
-
-    //Login Variables
-	$scope.loginInfo = {
-		email : undefined,
-		password : undefined
-	};
-
-
-	
-	//functions
-	$scope.logIn = function () {
-		var data = {
-			email : $scope.loginInfo.email,
-			password : $scope.loginInfo.password
-		};
-		$http.post("models/login.php", data).success(function(response){
-            if(response != 0){
-                //Login Successfull.
-                //Close the login dialog
-                //Relocate to an authorised page
-
-                console.log(response);
-                localStorage.setItem("user", JSON.stringify({user : response[0].name}));
-            }else{
-                //Notify user of wrong login details
-                console.log("Not Logged In");
-            }
-			
-
-		}).error(function(error){
-			console.error(error);
-		});
-        
-    };
-	//Init
-
-    //New User Info
-    $scope.userInfo = {
-        firstname : undefined,
-        lastname : undefined,
-        company : undefined,
-        email : undefined,
-        password : undefined,
-        confirmPass : undefined
-    };
-
-    //function
-    $scope.register = function () {
-        var data = {
-            firstname : $scope.userInfo.firstname,
-            lastname : $scope.userInfo.lastname,
-            company : $scope.userInfo.company,
-            email : $scope.userInfo.email,
-            password : $scope.userInfo.password,
-            confirmPass : $scope.userInfo.confirmPass
+        $scope.message = "";
+        //Login Variables
+        $scope.loginInfo = {
+            email: undefined,
+            password: undefined
         };
-        $http.post("models/register.php", data).success(function(response){
-            if(response == 1){
-                //Successfull Registration
-                //Close the Registration Dialog
-            }else{
-                //Unsuccessful registration
-                //Inform user to try again
+        //functions
+        $scope.logIn = function () {
+            var data = {
+                email: $scope.loginInfo.email,
+                password: $scope.loginInfo.password
+            };
+            $http.post("models/login.php", data).success(function (response) {
+                if (response != 0) {
+                    $scope.message = "You have successfully logged in!";
+                    setTimeout(function () {
+                        $mdDialog.hide();
+                    }, 2000);
+                    //Login Successfull.
+                    //Close the login dialog
+                    //Relocate to an authorised page
+
+                    console.log(response);
+                    localStorage.setItem("user", JSON.stringify({
+                        user: response[0].name
+                    }));
+                } else {
+                    //Notify user of wrong login details
+                    $scope.message = "Username or password is not valid. Try again!";
+                    console.log("Not Logged In");
+                }
+
+
+            }).error(function (error) {
+                console.error(error);
+            });
+
+        };
+        //Init
+
+        //Registration Variables
+        $scope.userInfo = {
+            firstname: undefined,
+            lastname: undefined,
+            company: undefined,
+            email: undefined,
+            password: undefined,
+            confirmPass: undefined
+        };
+
+        //function
+        $scope.register = function () {
+
+            var data = {
+                firstname: $scope.userInfo.firstname,
+                lastname: $scope.userInfo.lastname,
+                company: $scope.userInfo.company,
+                email: $scope.userInfo.email,
+                password: $scope.userInfo.password,
+                confirmPass: $scope.userInfo.confirmPass
+            };
+            if ($scope.userInfo.password == $scope.userInfo.confirmPass) {
+                $http.post("models/register.php", data).success(function (response) {
+                    if (response == 1) {
+                        //Successfull Registration
+                        $scope.message = "You have succesfully registered!"
+                        //Close the Registration Dialog
+                        setTimeout(function () {
+                            $mdDialog.hide();
+                        }, 2000);
+                    } else {
+                        //Unsuccessful registration
+                        //Inform user to try again
+                        $scope.message = "Something is wrong. Try again!";
+                    }
+                    console.log(response);
+
+                }).error(function (error) {
+                    console.error(error);
+                });
+            } else {
+                $scope.message = "Password does not match the confirm password. Try again!";
             }
-            console.log(response);
-
-        }).error(function(error){
-            console.error(error);
-        });
+        };
     };
-};
 
 
 
+}]);
 
 
 controllers.controller('testCtrl', ['$scope', '$log', '$http', 'ModelData', 'workspaces', function ($scope, $log, $http, ModelData, olData) {
@@ -382,7 +400,7 @@ controllers.controller('RESTgeo', ['$scope', '$http', 'workspaces', function ($s
     /*workspaces.getWorkspaces().success(function (data) {
         console.log(data);
     });*/
-  
+
 }]);
 
 
