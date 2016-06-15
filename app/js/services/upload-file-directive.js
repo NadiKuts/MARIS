f89dae29fe264b9ce9d2af0d6fb41c88d3373493
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2016-06-15 18:12:05 +0200

    uploading new files, submit button. They are visible only then, when in the JSON file this input has value 'true' of parameter 'change'

---
+++ app/js/services/upload-file-directive.js
@@ -0,0 +1,39 @@
+var controllers = angular.module('maris');
+
+controllers.controller('UploadCtrl', function ($scope) {
+
+}).directive('apsUploadFile', apsUploadFile);
+
+function apsUploadFile() {
+    var directive = {
+        restrict: 'E',
+        template: '<input id="fileInput" type="file" class="ng-hide"> <md-button id="uploadButton" class="md-raised md-primary" aria-label="attach_file">    Upload new file </md-button><md-input-container  md-no-float>    <input id="textInput" ng-model="fileName" type="text" placeholder="No file chosen" ng-readonly="true"></md-input-container>',
+        link: apsUploadFileLink
+    };
+    return directive;
+}
+
+function apsUploadFileLink(scope, element, attrs) {
+    var input = $(element[0].querySelector('#fileInput'));
+    var button = $(element[0].querySelector('#uploadButton'));
+    var textInput = $(element[0].querySelector('#textInput'));
+
+    if (input.length && button.length && textInput.length) {
+        button.click(function (e) {
+            input.click();
+        });
+        textInput.click(function (e) {
+            input.click();
+        });
+    }
+
+    input.on('change', function (e) {
+        var files = e.target.files;
+        if (files[0]) {
+            scope.fileName = files[0].name;
+        } else {
+            scope.fileName = null;
+        }
+        scope.$apply();
+    });
+}
